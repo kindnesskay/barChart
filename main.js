944d2b4a0dd36bfd8a0b0757699850c083a13700
kay <115101887+kindnesskay@users.noreply.github.com>
2023-06-16 20:29:00 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("myChart");
-let ctx = canvas.getContext("2d");
-ctx.fillStyle = "hsl(10, 79%, 65%)";
-
-const n = [120, 40, 10, 400];
-let days = [17.45, 34.91, 52.36, 3.07, 23.39, 43.28, 25.48];
-drawChart(days, canvas);
-console.log(canvas);
